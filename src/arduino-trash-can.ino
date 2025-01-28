e3e0843d638ca4519a66907cd60687a1f3ff355a
alfamirio <alfamirio@gmail.com>
2025-01-28 23:58:16 +0100

    Initial version

---
+++ src/arduino-trash-can.ino
@@ -0,0 +1,18 @@
+/*
+  Arduino code for a trash can that automatically opens its lid.
+  
+  This Arduino code controls an SG90 servo motor based on readings from an ultrasonic distance sensor. 
+
+  Board: Arduino Uno R3
+  Components: Servo motor(SG90) and Ultrasonic distance Sensor(HC-SR04)
+*/
+
+
+void setup() {
+
+}
+
+void loop() {
+
+}
+
